cbbc37561976d503df20a014a937bcce54f8ea3d
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-10-07 13:03:35 +0200

    Export and include types in docs

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 export * from './abi';
 export * from './errors';
+export * from './types';
+
+export type { BytesLike, NumberLike } from '@metamask/utils';
+export type { BooleanLike, FunctionLike, SolidityFunction } from './parsers';
